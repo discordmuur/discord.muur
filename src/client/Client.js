e6264e36b94ba50ffaa42e61cb719c7d03b3df9d
Kolkies <kolkies@kolkiesdev.nl>
2018-11-15 08:13:50 +0100

    Some JSDoc lines for the docs were not right

--- src/client/Client.js
+++ src/client/Client.js
@@ -11,20 +11,27 @@ class Client {
    * The constructor, here we will set most of the default settings for the client.
    */
   constructor(options = {}) {
+    /**
+     * 
+     */
     this.events = eventEmitter;
+    /**
+     * 
+     */
+    this.token = null;
   }
 
   /**
    * This endpoint is used to log the client into the gateway of discord
-   * @param {String} token is the Bot Token
-   * @param {Boolean} debug, states if the package will log debug messages to console. defaults to false
+   * @param {String} token This is the token of your bot
+   * @param {Boolean} debug This states if the package will log debug messages to console. defaults to false
    */
   login(token, debug = false) {
     this.token = token;
     WebSocket.connect(token);
 
     /*
-    * Here we will define all properties on the client.
+    * Here we will define all properties of the client.
     */
 
   }
